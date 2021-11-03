7b1cf09da147eec17d86102c71be8696e25fc1bd
emreusun <emre95neo@gmail.com>
2021-11-03 14:41:08 -0400

    new

--- js/components/TheThumbNail.js
+++ js/components/TheThumbNail.js
@@ -1,13 +1,36 @@
 export default {
     name: "TheThumbnailComponent",
 
+    props: ["piece"],
+
+   /*  created: function() {
+        console.log(message);
+    }, */
+
     template: `
-    <div class="bio-panel">
+    <div @click="showmydata" class="bio-panel">
         <div class="p_avatar">
-            <img src="" alt="">
+            <img :src='"images/" + piece.biopic' alt="piece.name">
         </div>
 
-        <h2 class="p_name">slug</h2>
+        <h2 class="p_name">{{ piece.name }}</h2>
+        <h3 class="p_role">{{ piece.role }}</h3>
     </div>
-    `
+    `,
+   /*  data: function () {
+        return {
+            message: " I am a component",
+            isVisible: false
+        }
+    }, */
+
+    methods : {
+       /*  showMe() {
+            this.isVisible = true;
+        } */
+        showmydata() {
+            debugger;
+            this.$emit("showdata", this.piece);
+        }
+    }
 }
--- js/main.js
+++ js/main.js
@@ -13,10 +13,25 @@ import TheThumbNail from "./components/TheThumbNail.js";
         data: {
             // this is storing the database info from our fetch call
             portfolioData: [],
-            message: "hello from Vue"
+            message: "hello from Vue",
+            isVisible: false,
+            currentPortfolioItem = {}
         },
 
         methods: {
+            popLightBox(item) {
+                debugger;
+                let portHeader = document.querySelector(".port_title");
+
+                //portHeader.textContent = item.name;
+                tihs.currentPortfolioItem =item;
+            },
+
+            toggleLightbox() {
+                this.isVisible = true;
+   
+            }
+
 
         },
 
